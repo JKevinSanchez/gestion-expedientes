15ebf0ddef542bce2e78f600d44bc96476ca3d61
marlopezsanchez27-creator <marlopezsanchez27@gmail.com>
2026-05-09 11:39:45 +0200

    Añadir la función validar_id

--- validaciones.py
+++ validaciones.py
@@ -5,3 +5,15 @@ def limpiar_texto(texto):
     if not isinstance(texto, str):
         return str(texto)
     return texto.replace(";", "").replace(":", "").strip()
+
+def validar_id(id_str):
+    """
+    Valida que el ID sea un número entero positivo.
+    """
+    try:
+        id_num = int(id_str)
+        if id_num <= 0:
+            return False, "El ID debe ser un número entero positivo."
+        return True, id_num
+    except ValueError:
+        return False, "El ID debe ser un número válido."
